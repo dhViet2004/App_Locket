332af5aa21f105cb3dd6269124d03992b487c624
VoPhuocViet1909 <22730761.viet@student.iuh.edu.vn>
2025-11-22 12:53:14 +0700

    admin

--- Backend/src/routes/admin.routes.ts
+++ Backend/src/routes/admin.routes.ts
@@ -3,6 +3,8 @@ import { requireAuth } from '../middlewares/auth.middleware';
 import { authorize } from '../middlewares/authorize';
 import { adminUserController } from '../controllers/admin_user.controller';
 import { adminPostController } from '../controllers/admin_post.controller';
+import { adminPlanController } from '../controllers/admin_plan.controller';
+import { adminRefundController } from '../controllers/admin_refund.controller';
 import { list, getById } from '../controllers/admin_audit_log.controller';
 
 const router = Router();
@@ -15,6 +17,15 @@ router.put('/users/:userId/unban', (req, res, next) => adminUserController.unban
 
 // Content Management
 router.delete('/posts/:postId', (req, res, next) => adminPostController.deletePost(req, res, next));
+
+// Plan Management
+router.post('/plans', (req, res, next) => adminPlanController.createPlan(req, res, next));
+router.put('/plans/:planId', (req, res, next) => adminPlanController.updatePlan(req, res, next));
+router.delete('/plans/:planId', (req, res, next) => adminPlanController.deactivatePlan(req, res, next));
+router.put('/plans/:planId/activate', (req, res, next) => adminPlanController.activatePlan(req, res, next));
+
+// Refund Management
+router.put('/refunds/:refundId/process', (req, res, next) => adminRefundController.handleRefund(req, res, next));
 
 // Audit Logs
 router.get('/audit-logs', list);
